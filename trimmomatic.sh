79315aa6ba7498f37e13b61db44d296d25bf263e
Steven Kitchens - Auburn University <aubsrk@dmcvlogin2.asc.edu>
2020-08-19 18:03:21 -0500

    updated variables and command for trimmomatic

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -5,12 +5,12 @@
 #supported, and are identified by use of ".gz" or ".bz" extensions. 
 #####################Variables############################
 file=SRR1752832
-#path=~/scripts/
+file1=SRR1752832_1
+file2=SRR1752832_2
 ##########  load the module
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load anaconda/3-2018.12
 ########## commands go here
-#java -jar /opt/asn/apps/anaconda_3-2018.12/share/trimmomatic/trimmomatic.jar PE -threads 4  -phred33 -trimlog ${path}/trimlog.txt ${path}/${file}_1.fastq.gz ${path}/${file}_2.fastq.gz ${path}/${file}_1.trim.fastq ${path}/${file}_2.trim.fastq SLIDINGWINDOW:4:20 MINLEN:36
-java -jar /opt/asn/apps/anaconda_3-2018.12/share/trimmomatic/trimmomatic.jar -phred33 PE ${file}_1.fastq ${file}_2/fastq ${file}_1_trim.fastq ${file}_2_trim.fastq ILLUMINACLIP:/opt/asn/apps/anaconda_3-2018.12/share/trimmomatic/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
+java -jar /opt/asn/apps/anaconda_3-2018.12/share/trimmomatic/trimmomatic.jar PE -phred33 -trimlog $file.trimlog $file1.fastq.gz $file2.fastq.gz ${file1}.trim.fastq.gz ${file2}.trim.fast.gz SLIDINGWINDOW:4:20 MINLEN:36
 #
 exit
